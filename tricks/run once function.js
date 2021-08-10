0c3b55c31b917eb2603effb15728a5a373c9abe0
KILLER.CODES <Killercodes@users.noreply.github.com>
2021-08-11 00:26:32 +0530

    Create run once function.js

---
+++ tricks/run once function.js
@@ -0,0 +1,18 @@
+//run once function
+function RunOnce(fn, context) { 
+	var result;
+
+	return function() { 
+		if(fn) {
+			result = fn.apply(context || this, arguments);
+			fn = null;
+		}
+
+		return result;
+	};
+}
+
+// Usage
+var canOnlyFireOnce = RunOnce(function() {
+	console.log('Fired!');
+});
